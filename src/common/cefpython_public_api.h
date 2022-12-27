170cdd1645fdb40183674f3cfc3a062ba7e4aa4b
llcc01 <2417224420@qq.com>
2022-12-27 18:00:22 +0800

    fix

--- src/common/cefpython_public_api.h
+++ src/common/cefpython_public_api.h
@@ -50,6 +50,12 @@
 #include "../../build/build_cefpython/cefpython_py38_fixed.h"
 #elif PY_MINOR_VERSION == 9
 #include "../../build/build_cefpython/cefpython_py39_fixed.h"
+#elif PY_MINOR_VERSION == 10
+#include "../../build/build_cefpython/cefpython_py310_fixed.h"
+#elif PY_MINOR_VERSION == 11
+#include "../../build/build_cefpython/cefpython_py311_fixed.h"
+#else
+#error "Unsupported Python version, please check cefpython_public_api.h"
 #endif // PY_MINOR_VERSION
 #endif // PY_MAJOR_VERSION
 
